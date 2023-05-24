a8ba9246175f56d372c306f7b20a9e127c53add6
Michelle Abt <mabt924@gmail.com>
2023-05-23 20:08:55 -0400

    Initial Commit

---
+++ AbtToDoList/AbtToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbtToDoListApp.swift
+//  AbtToDoList
+//
+//  Created by Michelle Abt on 5/23/23.
+//
+
+import SwiftUI
+
+@main
+struct AbtToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
